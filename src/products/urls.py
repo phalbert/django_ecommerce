49c1e1460205e6698dc265f955aaeb9913ef6b08
hpal <ahosea15@gmail.com>
2020-03-24 23:44:01 +0300

    added slug field to the product model

--- src/products/urls.py
+++ src/products/urls.py
@@ -6,5 +6,6 @@ app_name = 'products'
 urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='product-list'),
     path('products/featured', views.ProductFeaturedListView.as_view(), name='featured-list'),
-    path('products/<slug:pk>/', views.ProductDetailView.as_view(), name='product-detail')
+    # path('products/<slug:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('products/<slug:slug>/', views.ProductDetailView.as_view(), name='product-slug')
 ]

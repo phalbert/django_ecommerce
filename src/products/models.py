a17f9e9d57d5475147e914f60b79cf5f923b5ead
hpal <ahosea15@gmail.com>
2020-03-24 13:42:42 +0300

    added image field to product model

--- src/products/models.py
+++ src/products/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=0)
-    image = models.FileField(upload_to=upload_image_path, null=True, blank=True) # uploaded to media root
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True) # uploaded to media root
 
     def __str__(self):
         return self.title
